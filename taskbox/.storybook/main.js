d15c7bcd27349241ab49880e1551e3837f4ec088
[name] <tlsrlqkdrl91@naver.com>
2024-02-23 15:11:59 +0900

    js 변경

--- taskbox/.storybook/main.js
+++ taskbox/.storybook/main.js
@@ -1,18 +1,21 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
-};
-export default config;
+module.exports = {
+   stories: [
+     '../src/**/*.stories.mdx',
+     '../src/**/*.stories.@(js|jsx|ts|tsx)'
+   ],
+   stories: ['../src/components/**/*.stories.js'],
+    staticDirs: ['../public'],
+    addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      '@storybook/preset-create-react-app',
+      '@storybook/addon-interactions',
+    ],
+    features: {
+      postcss: false,
+    },
+    framework: '@storybook/react',
+    core: {
+      builder: 'webpack4',
+    },
+  };
